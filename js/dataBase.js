684f4b64aaa1450308ef4a50c1891d0dce4ed29c
Diogo França <diogofranca845@gmail.com>
2022-01-09 18:42:50 -0300

    feat: adicionado objeto com dados padrão da questão inicial.

---
+++ js/dataBase.js
@@ -0,0 +1,177 @@
+export const standardQuestion = {
+  question: 'Oi eu sou o goku',
+  answers: [
+    {
+      answer: 'Dragon',
+      status: false
+    },
+    {
+      answer: 'Avião',
+      status: false
+    },
+    {
+      answer: 'Gigante',
+      status: true
+    },
+    {
+      answer: 'Borboleta',
+      status: false
+    },
+    {
+      answer: 'Dinossauro',
+      status: false
+    },
+    {
+      answer: 'Vacc',
+      status: false
+    }
+  ]
+}
+
+export const questions = [
+  {
+    question: 'Quem tipo de raça hagrid é ?',
+    answers: [
+      {
+        answer: 'Dragon',
+        status: false
+      },
+      {
+        answer: 'Avião',
+        status: false
+      },
+      {
+        answer: 'Gigante',
+        status: false
+      },
+      {
+        answer: 'Borboleta',
+        status: false
+      },
+      {
+        answer: 'Dinossauro',
+        status: false
+      },
+      {
+        answer: 'Vacc',
+        status: false
+      }
+    ]
+  },
+  {
+    question: 'Quem tipo de raça hagrid é ?',
+    answers: [
+      {
+        answer: 'Dragon',
+        status: false
+      },
+      {
+        answer: 'Avião',
+        status: false
+      },
+      {
+        answer: 'Gigante',
+        status: false
+      },
+      {
+        answer: 'Borboleta',
+        status: false
+      },
+      {
+        answer: 'Dinossauro',
+        status: false
+      },
+      {
+        answer: 'Vacc',
+        status: false
+      }
+    ]
+  },
+  {
+    question: 'Quem tipo de raça hagrid é ?',
+    answers: [
+      {
+        answer: 'Dragon',
+        status: false
+      },
+      {
+        answer: 'Avião',
+        status: false
+      },
+      {
+        answer: 'Gigante',
+        status: false
+      },
+      {
+        answer: 'Borboleta',
+        status: false
+      },
+      {
+        answer: 'Dinossauro',
+        status: false
+      },
+      {
+        answer: 'Vacc',
+        status: false
+      }
+    ]
+  },
+  {
+    question: 'Quem tipo de raça hagrid é ?',
+    answers: [
+      {
+        answer: 'Dragon',
+        status: false
+      },
+      {
+        answer: 'Avião',
+        status: false
+      },
+      {
+        answer: 'Gigante',
+        status: false
+      },
+      {
+        answer: 'Borboleta',
+        status: false
+      },
+      {
+        answer: 'Dinossauro',
+        status: false
+      },
+      {
+        answer: 'Vaca',
+        status: false
+      }
+    ]
+  },
+  {
+    question: 'Quem tipo de raça hagrid é ?',
+    answers: [
+      {
+        answer: 'Dragon',
+        status: false
+      },
+      {
+        answer: 'Avião',
+        status: false
+      },
+      {
+        answer: 'Gigante',
+        status: false
+      },
+      {
+        answer: 'Borboleta',
+        status: false
+      },
+      {
+        answer: 'Dinossauro',
+        status: false
+      },
+      {
+        answer: 'Vacc',
+        status: false
+      }
+    ]
+  }
+]
